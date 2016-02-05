eb73425baa0f3bc0dd081ebf1f5a4c7602d65cc6
choupijiang <zhangshenghucn@gmail.com>
2016-02-05 18:57:50 +0800

    add

---
+++ blog/migrations/0002_auto_20160203_1954.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='post',
+            options={'ordering': ('-publish',)},
+        ),
+        migrations.RenameField(
+            model_name='post',
+            old_name='published',
+            new_name='publish',
+        ),
+    ]
